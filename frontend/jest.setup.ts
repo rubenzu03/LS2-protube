ed17690be809f0cf59346bc62d54d64b3fd63832
cakeruu <150198610+cakeruu@users.noreply.github.com>
2025-10-06 12:40:18 +0200

    Initialization of the frontend

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -5,8 +5,8 @@ import fetchMock from 'jest-fetch-mock';
 jest.mock('./src/utils/Env', () => ({
   getEnv: () => ({
     API_BASE_URL: 'other',
-    MEDIA_BASE_URL: 'development',
-  }),
+    MEDIA_BASE_URL: 'development'
+  })
 }));
 
 fetchMock.enableMocks();
