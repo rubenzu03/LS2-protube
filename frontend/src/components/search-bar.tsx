7ba31f1615832e4d103e5d2c51fb0b77324136e8
cakeruu <150198610+cakeruu@users.noreply.github.com>
2025-11-10 10:27:55 +0100

    Update search-bar.tsx

--- frontend/src/components/search-bar.tsx
+++ frontend/src/components/search-bar.tsx
@@ -2,7 +2,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export function SearchBar() {
   return (
-    <div className="flex items-stretch rounded-full border w-96 bg-secondary/20 overflow-hidden ">
+    <div className="flex items-stretch rounded-full border w-96 bg-secondary/20 overflow-hidden">
       <input type="text" placeholder="Search" className="flex-1 bg-transparent px-4 py-2 outline-none placeholder:text-muted-foreground" />
       <button className="flex items-center justify-center border-l px-4 cursor-pointer bg-accent">
         <MagnifyingGlassIcon className="h-5 w-5" />
