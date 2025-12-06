f3e185b7f37214f4c65dfd841db4635f65bd33d5
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-12-06 19:17:59 +0100

    Video upload tests

--- backend/src/main/java/com/tecnocampus/LS2/protube_back/persistence/dto/VideoDTO.java
+++ backend/src/main/java/com/tecnocampus/LS2/protube_back/persistence/dto/VideoDTO.java
@@ -2,7 +2,6 @@ package com.tecnocampus.LS2.protube_back.persistence.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-@JsonDeserialize(using = VideoDTODeserializer.class)
 public record VideoDTO(
         Long id,
         String title,
