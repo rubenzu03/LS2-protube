b9e90c382e0e8cb981e955702bf885b88f5d9be2
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2025-12-07 19:27:31 +0100

    Docker DB connection fix

---
+++ backend/src/test/java/ws/schild/jave/info/VideoSize.java
@@ -0,0 +1,28 @@
+package ws.schild.jave.info;
+
+// Minimal test stub for VideoSize
+public class VideoSize {
+    private int width;
+    private int height;
+
+    public VideoSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
